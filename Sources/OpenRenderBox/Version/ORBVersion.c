d582f042a222e6bceece6b33067249e380be7964
Kyle <kyle201817146@gmail.com>
2025-09-08 02:08:44 +0800

    Fix warning

--- Sources/OpenRenderBox/Version/ORBVersion.c
+++ Sources/OpenRenderBox/Version/ORBVersion.c
@@ -5,6 +5,6 @@
 #include <OpenRenderBox/ORBVersion.h>
 
 #if OPENRENDERBOX_RELEASE == OPENRENDERBOX_RELEASE_2024
-const double OpenRenderBoxVersionNumber __attribute__ ((used)) = (double)6.0;
+double OpenRenderBoxVersionNumber __attribute__ ((used)) = (double)6.0;
 const unsigned char OpenRenderBoxVersionString[] __attribute__ ((used)) = "@(#)PROAGRAM:OpenRenderBox  PROJECT:OpenRenderBox-6.0.87\n";
 #endif
