14166e244c2a2eeaea8eaff37c7dc5de0c8c9db4
Kyle <kyle201817146@gmail.com>
2025-12-27 21:01:24 +0800

    Add C23 check for static_assert without message

--- Sources/OpenRenderBox/include/OpenRenderBox/ORBPathCallbacks.h
+++ Sources/OpenRenderBox/include/OpenRenderBox/ORBPathCallbacks.h
@@ -46,18 +46,21 @@ typedef struct ORB_SWIFT_NAME(ORBPath.Callbacks) ORBPathCallbacks {
     const struct ORBPathCallbacks * _Nullable (* _Nullable next)(const void *object);
 } ORBPathCallbacks;
 
-static_assert(sizeof(ORBPathCallbacks) == 0x58);
-static_assert(offsetof(ORBPathCallbacks, flags) == 0x00);
-static_assert(offsetof(ORBPathCallbacks, retain) == 0x08);
-static_assert(offsetof(ORBPathCallbacks, release) == 0x10);
-static_assert(offsetof(ORBPathCallbacks, apply) == 0x18);
-static_assert(offsetof(ORBPathCallbacks, isEqual) == 0x20);
-static_assert(offsetof(ORBPathCallbacks, isEmpty) == 0x28);
-static_assert(offsetof(ORBPathCallbacks, isSingleElement) == 0x30);
-static_assert(offsetof(ORBPathCallbacks, bezierOrder) == 0x38);
-static_assert(offsetof(ORBPathCallbacks, boundingRect) == 0x40);
-static_assert(offsetof(ORBPathCallbacks, cgPath) == 0x48);
-static_assert(offsetof(ORBPathCallbacks, next) == 0x50);
+// static_assert without message requires C23 or C++17
+#if defined(__cplusplus) || (defined(__STDC_VERSION__) && __STDC_VERSION__ >= 202311L)
+static_assert(sizeof(struct ORBPathCallbacks) == 0x58);
+static_assert(offsetof(struct ORBPathCallbacks, flags) == 0x00);
+static_assert(offsetof(struct ORBPathCallbacks, retain) == 0x08);
+static_assert(offsetof(struct ORBPathCallbacks, release) == 0x10);
+static_assert(offsetof(struct ORBPathCallbacks, apply) == 0x18);
+static_assert(offsetof(struct ORBPathCallbacks, isEqual) == 0x20);
+static_assert(offsetof(struct ORBPathCallbacks, isEmpty) == 0x28);
+static_assert(offsetof(struct ORBPathCallbacks, isSingleElement) == 0x30);
+static_assert(offsetof(struct ORBPathCallbacks, bezierOrder) == 0x38);
+static_assert(offsetof(struct ORBPathCallbacks, boundingRect) == 0x40);
+static_assert(offsetof(struct ORBPathCallbacks, cgPath) == 0x48);
+static_assert(offsetof(struct ORBPathCallbacks, next) == 0x50);
+#endif
 
 /// Extended callbacks structure with additional extended callbacks argument
 typedef struct ORB_SWIFT_NAME(ORBPath.CallbacksExtended) ORBPathCallbacksExtended {
@@ -78,18 +81,21 @@ typedef struct ORB_SWIFT_NAME(ORBPath.CallbacksExtended) ORBPathCallbacksExtende
     const struct ORBPathCallbacksExtended * _Nullable (* _Nullable next)(const void *object, const struct ORBPathCallbacksExtended *extended);
 } ORBPathCallbacksExtended;
 
-static_assert(sizeof(ORBPathCallbacksExtended) == 0x58);
-static_assert(offsetof(ORBPathCallbacksExtended, flags) == 0x00);
-static_assert(offsetof(ORBPathCallbacksExtended, retain) == 0x08);
-static_assert(offsetof(ORBPathCallbacksExtended, release) == 0x10);
-static_assert(offsetof(ORBPathCallbacksExtended, apply) == 0x18);
-static_assert(offsetof(ORBPathCallbacksExtended, isEqual) == 0x20);
-static_assert(offsetof(ORBPathCallbacksExtended, isEmpty) == 0x28);
-static_assert(offsetof(ORBPathCallbacksExtended, isSingleElement) == 0x30);
-static_assert(offsetof(ORBPathCallbacksExtended, bezierOrder) == 0x38);
-static_assert(offsetof(ORBPathCallbacksExtended, boundingRect) == 0x40);
-static_assert(offsetof(ORBPathCallbacksExtended, cgPath) == 0x48);
-static_assert(offsetof(ORBPathCallbacksExtended, next) == 0x50);
+// static_assert without message requires C23 or C++17
+#if defined(__cplusplus) || (defined(__STDC_VERSION__) && __STDC_VERSION__ >= 202311L)
+static_assert(sizeof(struct ORBPathCallbacksExtended) == 0x58);
+static_assert(offsetof(struct ORBPathCallbacksExtended, flags) == 0x00);
+static_assert(offsetof(struct ORBPathCallbacksExtended, retain) == 0x08);
+static_assert(offsetof(struct ORBPathCallbacksExtended, release) == 0x10);
+static_assert(offsetof(struct ORBPathCallbacksExtended, apply) == 0x18);
+static_assert(offsetof(struct ORBPathCallbacksExtended, isEqual) == 0x20);
+static_assert(offsetof(struct ORBPathCallbacksExtended, isEmpty) == 0x28);
+static_assert(offsetof(struct ORBPathCallbacksExtended, isSingleElement) == 0x30);
+static_assert(offsetof(struct ORBPathCallbacksExtended, bezierOrder) == 0x38);
+static_assert(offsetof(struct ORBPathCallbacksExtended, boundingRect) == 0x40);
+static_assert(offsetof(struct ORBPathCallbacksExtended, cgPath) == 0x48);
+static_assert(offsetof(struct ORBPathCallbacksExtended, next) == 0x50);
+#endif
 
 #if ORB_TARGET_OS_DARWIN
 /// Global callbacks for CGPath-backed paths
